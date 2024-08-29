88ff8fc8fa9ce599ac5633916507d7a0c7a6e7cc
KingMing <alswl7754@gmail.com>
2024-08-29 11:46:06 +0900

    8/29 : week10 - pgs42583

---
+++ Algorithm/src/week10/pgs42584.java
@@ -0,0 +1,44 @@
+package week10;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class pgs42584 {
+    public static void main(String[] args) {
+        int[] result = solution(new int[]{2,1});
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] solution(int[] prices) {
+        int[] result = new int[prices.length];
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < prices.length; i++) {
+            q.add(prices[i]);
+        }
+
+        int idx = 0;
+        while (!q.isEmpty()) {
+            int price = q.poll();
+            result[idx] = 0;
+
+            for (int j = 0; j < q.size(); j++) {
+                if (!q.isEmpty()) {   // 1초는 값이 유지될 수 있음
+                    result[idx]++;
+                    int nextPrice = q.poll();
+                    q.add(nextPrice);
+                    if (price > nextPrice) {    // 값이 떨어짐
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            idx++;
+        }
+
+        return result;
+    }
+}
