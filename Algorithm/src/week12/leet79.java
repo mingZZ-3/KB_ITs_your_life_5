c12136e0014f6515a9ad5a45855e1db24c80e373
KingMing <alswl7754@gmail.com>
2024-09-08 11:59:57 +0900

    9/8 : week12 - leet79

---
+++ Algorithm/src/week12/leet79.java
@@ -0,0 +1,65 @@
+package week12;
+
+public class leet79 { // word search
+
+    class Solution {
+        int R, C;
+        int[] dirR = {-1, 1, 0, 0};
+        int[] dirC = {0, 0, -1, 1};
+        boolean isExist = false;
+        boolean[][] visited;
+
+        public boolean exist(char[][] board, String word) {
+            R = board.length;
+            C = board[0].length;
+            visited = new boolean[R][C];
+
+            for (int i=0; i < R; i++) {
+                for (int j=0; j < C; j++) {
+                    if (board[i][j] == word.charAt(0) && !visited[i][j]) { // word의 시작과 같은 문자면
+                        visited[i][j] = true;
+                        dfs(board, 0, new Node(i, j), word);
+                        visited[i][j] = false;
+                    }
+
+                    if (isExist) return true;
+                }
+            }
+            return false;
+        }
+
+        public void dfs(char[][] board, int idx, Node now, String target) {
+            if (idx == target.length() - 1)  {
+                isExist = true;
+                return;
+            }
+
+            // 상하좌우 확인
+            for (int i=0; i < 4; i++) {
+                int nr = now.r + dirR[i];
+                int nc = now.c + dirC[i];
+
+                if (InRange(nr, nc) && board[nr][nc] == target.charAt(idx + 1)){   // 다음 문자와 동일하면
+                    if (!visited[nr][nc]){
+                        visited[nr][nc] = true;
+                        dfs(board, idx + 1, new Node(nr, nc), target);
+                        visited[nr][nc] = false;
+                    }
+                }
+            }
+        }
+
+        boolean InRange(int r, int c) {
+            return (r >= 0) && (r < R) && (c >= 0) && (c < C);
+        }
+
+        class Node {
+            int r, c;
+
+            Node(int r, int c) {
+                this.r = r;
+                this.c = c;
+            }
+        }
+    }
+}
